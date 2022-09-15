e294ee4badf06b1bd8c9e330b6b899f6da967c1d
sruthigopi <101387050+sruthigopi@users.noreply.github.com>
2022-09-15 16:49:40 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashbordComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
